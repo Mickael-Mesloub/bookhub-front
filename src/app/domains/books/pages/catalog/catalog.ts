c7828e357297078e940496036682616963ec0b3f
Violaine Tzl <201018250+iamviolainetzl@users.noreply.github.com>
2026-04-24 16:29:38 +0200

    adding app-book-search component inside catalog component

--- src/app/domains/books/pages/catalog/catalog.ts
+++ src/app/domains/books/pages/catalog/catalog.ts
@@ -4,19 +4,20 @@ import { BookService } from '../../services/book-service';
 import { Book, PageOfBooks } from '../../models/book-models';
 import { ApiResponse } from '../../../../config/api/api';
 import { Button } from '../../../../components/shared/button/button';
+import { BookSearch } from '../../components/book-search/book-search';
 
 
 @Component({
   selector: 'app-catalog',
-  imports: [BookCard, Button],
+  imports: [BookCard, Button, BookSearch],
   templateUrl: './catalog.html',
   styleUrl: './catalog.scss',
 })
 export class Catalog implements OnInit {
   bookService: BookService = inject(BookService);
   books: Book[] = [];
-  bookPage!: PageOfBooks
-  currentPage: number = 0
+  bookPage!: PageOfBooks;
+  currentPage: number = 0;
 
   ngOnInit() {
     this.fetchAllBooks();
@@ -25,25 +26,24 @@ export class Catalog implements OnInit {
   fetchAllBooks(page?: number, size?: number, sort?: string): void {
     this.bookService.getAllBooks(page, size, sort).subscribe({
       next: (response: ApiResponse<PageOfBooks>) => {
-        this.bookPage = response.data
-        this.books = response.data.content
-        this.currentPage = this.bookPage.number
+        this.bookPage = response.data;
+        this.books = response.data.content;
+        this.currentPage = this.bookPage.number;
       },
       error: (err) => console.error('Failed to fetch books: ', err),
     });
   }
 
-
-  nextPage(): void{
-    if(this.currentPage < this.bookPage.totalPages){
-      this.currentPage ++
+  nextPage(): void {
+    if (this.currentPage < this.bookPage.totalPages) {
+      this.currentPage++;
       this.fetchAllBooks(this.currentPage);
     }
   }
 
-  previousPage(): void{
-    if(this.currentPage > 0){
-      this.currentPage --
+  previousPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
       this.fetchAllBooks(this.currentPage);
     }
   }
