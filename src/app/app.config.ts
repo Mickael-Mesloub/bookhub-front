1288ad4194554ebf956273054eaf0ba91289e265
Mickaël Mesloub <mickael.mesloub@gmail.com>
2026-04-27 11:48:26 +0200

    clean + handle navigation error globally for resolver

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NavigationError, provideRouter, Router, withNavigationErrorHandler } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { NotificationService } from './components/shared/notification/service/notification-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideRouter(
+      routes,
+      withNavigationErrorHandler((error: NavigationError) => {
+        const router = inject(Router);
+        const notificationService = inject(NotificationService);
+        
+        if (error) {
+          console.error('Navigation error occurred:', error.error);
+          notificationService.openNotification({
+          type: 'alert-error',
+          message: error.error,
+        });
+        }
+        router.navigate(['/']);
+      }),
+    ),
+    provideHttpClient(withFetch()),
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppLayout } from './layout/app-layout/app-layout/app-layout';
-import { AuthService } from './domains/users/services/auth-service';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +8,4 @@ import { AuthService } from './domains/users/services/auth-service';
   styleUrl: './app.scss',
 })
 export class App {
-
-  private readonly authService = inject(AuthService); // injecter mon service
-
-  clickBtn(): void {
-    console.log('Button clicked!');
-  }
-
 }
